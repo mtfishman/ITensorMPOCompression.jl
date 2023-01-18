05e3561ac74143bf45967bc332e0e59d4bd1aae9
Jan Reimers <janreimers@gmail.com>
2023-01-18 16:48:01 -0600

    Get rid of old max_Dw function

--- src/characterization.jl
+++ src/characterization.jl
@@ -543,11 +543,6 @@ function is_upper_regular_form(H::AbstractMPS,eps::Float64=default_eps)::Bool
     return is_regular_form(H,upper,eps)
 end
 
-# function maxlinkdim(H::AbstractMPS)::Int64
-#     return maxlinkdim(H)
-#    return maximum(get_Dw(H))
-end
-
 function get_Dw(H::MPO)::Vector{Int64}
     N=length(H)
     Dws=Vector{Int64}(undef,N-1)
